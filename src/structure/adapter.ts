ab30653901f9c3241da27ba2bb9c579c7682002e
Maria Luna <mlunat20@gmail.com>
2025-08-29 16:03:45 -0500

    add example adapter of send email with sendGrid

--- src/structure/adapter.ts
+++ src/structure/adapter.ts
@@ -7,3 +7,35 @@ export interface EmailPayload {
 export interface EmailSender {
   sendEmail(payload: EmailPayload): Promise<void>;
 }
+
+// use case application
+export class SendWelcomeEmail {
+  constructor(private emailSender: EmailSender) {}
+
+  async execute(email: string) {
+    await this.emailSender.sendEmail({
+      to: email,
+      subject: "Welcome!",
+      body: "Thanks for joining 🚀",
+    });
+  }
+}
+
+// Adapter: integration with SendGrid
+export class SendGridAdapter implements EmailSender {
+  constructor(private apiKey: string) {
+    // sgMail.setApiKey(apiKey)
+  }
+
+  async sendEmail(payload: EmailPayload): Promise<void> {
+    // here you add mapper
+    const msg = {
+      to: payload.to,
+      from: "no-reply@myapp.com",
+      subject: payload.subject,
+      text: payload.body,
+    };
+    console.log("[SendGrid] would send:", msg);
+    // await sgMail.send(msg)
+  }
+}
